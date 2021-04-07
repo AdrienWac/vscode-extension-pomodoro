157d43f162c6a455f9e5a0134d72f11a88ba6357
LAPTOP-CBBSLR8Q\Adrien <adrienlambert725@msn.com>
2021-04-07 18:00:51 +0200

    - Encapsulation de l'ensemble du script dans la webView pour éviter que la variable vscode soit dans le scope global

--- src/extension.ts
+++ src/extension.ts
@@ -133,49 +133,54 @@ function getHtmlContent(cssFileUri: vscode.Uri): string
 
 			<script>
 				
-				const vscode = acquireVsCodeApi();
+				(function () {
 
-				var counter = document.getElementById('counter');
-				var count = 0;
-				var interval = undefined;
+					const vscode = acquireVsCodeApi();
+
+					var counter = document.getElementById('counter');
+					var count = 0;
+					var interval = undefined;
 
 
-				// Récupération des messages de l'extension
-				window.addEventListener('message', event => {
+					// Récupération des messages de l'extension
+					window.addEventListener('message', event => {
 
-					let message = event.data;
+						let message = event.data;
 
-					switch (message.command) {
-						
-						case 'start':
+						switch (message.command) {
 
-							interval = setInterval(() => {
+							case 'start':
 
-								counter.textContent = count++;
-								if (count%10 === 0) {
-									console.log(vscode);
-									vscode.postMessage({
-										command: 'alert',
-										text: 'Dont forget to stop the counter. His current value is ' + count
-									})
+								interval = setInterval(() => {
 
+									counter.textContent = count++;
+									if (count%10 === 0) {
+										console.log(vscode);
+										vscode.postMessage({
+											command: 'alert',
+											text: 'Dont forget to stop the counter. His current value is ' + count
+										})
+
+									}
+
+								}, 100);
+
+								break;
+
+							case 'stop':
+
+								if (interval) {
+									clearInterval(interval);
 								}
 
-							}, 100);
+								break;
 
-							break;
+						}
 
-						case 'stop':
+					});
 
-							if (interval) {
-								clearInterval(interval);
-							}
-
-							break;
-						
-					}
-
-				});
+				}())
+				
 
 			</script>
 
