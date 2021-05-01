7d22f3ba9884adffdf431cc4b0cc3f9cbbbf09b4
LAPTOP-CBBSLR8Q\Adrien <adrienlambert725@msn.com>
2021-05-01 17:49:11 +0200

    --- Factory --- - Suppression des fichiers

---
+++ src/view.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+export class View {
+
+    private context: vscode.ExtensionContext;
+
+    private 
+
+    constructor(context: vscode.ExtensionContext) {
+        this.context = context;
+    }
+
+
+
+}
