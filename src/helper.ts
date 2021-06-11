86f97b729b4a2b9993dbecbd1b73b69b240acc64
LAPTOP-CBBSLR8Q\Adrien <adrienlambert725@msn.com>
2021-06-11 12:47:35 +0200

    - Ajustement test fonction extract de la classe helper - Implémentation de la fonction extract de la classe helper

--- src/helper.ts
+++ src/helper.ts
@@ -5,6 +5,27 @@ export class Helper {
 
     static extract(obj: any, path: string): any {
 
+        // S'il n'y a pas d'acolade dans path
+
+            // Si pas de point dans path 
+                // On retourne obj[path]
+
+            // On explode le path avec le point en delimiter
+            
+
+            // Si plus de 2 étapes
+                // On parcourt les étapes jusqu'a la valeur
+                // On retourne la valeur
+
+            
+            // Sinon (2 étapes ) on retourne obj[explodePath[0]][explodePath[1]]
+
+        // Fin si
+
+
+
+        // On explode path 
+
         return 'oui';
 
     }
--- src/test/suite/helper.test.ts
+++ src/test/suite/helper.test.ts
@@ -1,14 +1,29 @@
 import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-// import * as vscode from 'vscode';
+import * as vscode from 'vscode';
 import { Helper } from '../../helper';
 
 suite('Helper Test', () => {
-	// vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual('oui', Helper.extract({test:'oui'}, 'non'));
+	const configurations = vscode.workspace.getConfiguration('pomodoroTimer');
+
+	let data = [
+		{
+			object: configurations,
+			path: 'work.duration.default',
+			exceptedResult: configurations.work.duration
+		}
+	];
+
+
+
+	test('Extract configuration value', () => {
+
+		data.forEach((item) => {
+			assert.strictEqual(item.exceptedResult, Helper.extract(item.object, item.path));
+		})
+
 	});
 
 });
