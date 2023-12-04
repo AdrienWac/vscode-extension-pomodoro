513eb6e0e0f1f945541f0ad69a8521dd611c95b1
Adrien <adrienlambert725@msn.com>
2023-12-04 23:07:25 +0100

    feature(init-vue-project) Start to implement timer component

---
+++ client/src/ui/Timer/Timer.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, computed } from 'vue'
+const count = ref(0);
+const minutes =  ref(25);
+const seconds = ref(0);
+const displayValue = computed(() => {
+  let time = minutes.value * 60 + seconds.value;
+  let timeToDisplay = time / 60;
+  let minuteToDisplay = parseInt(timeToDisplay);
+  let secondToDisplay = Math.round((time - minutes.value) * 60);
+  return minuteToDisplay+":"+secondToDisplay; 
+})
+</script>
+
+<template>
+  <div class="card">
+    <h1>Timer</h1>
+    {{ minutes }}:{{ seconds }}
+    {{ displayValue }}
+  </div>
+</template>
+
+<style scoped>
+h1{color: red;}
+</style>
