a7d79c8699383ed13295c899b89889ceab7bcd9d
Adrien <adrienlambert725@msn.com>
2023-12-20 22:14:52 +0100

    feat(create-item) Move controller symfony inside UI folder. Edit routes files

--- app/src/UI/HTTP/Web/Controller/PostItemController.php
+++ app/src/UI/HTTP/Web/Controller/PostItemController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\UI\HTTP\Web\Controller;
 
 use App\Application\Controller\CreateItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,9 +9,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PostItemController extends AbstractController
 {
-    public function index(Request $request, CreateItemInterface $postCreateItemApplication): Response
+    public function index(Request $request): Response
     {
-        $postItemCreateResponse = $postCreateItemApplication->create($request);
+        // $postItemCreateResponse = $postCreateItemApplication->create($request);
         // Transform  $postItemCreateResponse to Symfony Response
         return new Response;
     }
