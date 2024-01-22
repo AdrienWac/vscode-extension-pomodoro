5e8f7a55079299538a247df4574ed44352e77ea4
Adrien <adrienlambert725@msn.com>
2024-01-22 07:39:30 +0100

    feat(create-api) Create script for generate server with open api generator and create post item controller for rest api

---
+++ app/src/UI/HTTP/REST/Swagger/build.sh
@@ -0,0 +1,7 @@
+# Build source with OpenAPI generator. Use Docker image.
+docker run --rm -v "/home/adrien/Projets/vscode-extension-pomodoro/app/src/UI/HTTP/REST/Swagger:/local" openapitools/openapi-generator-cli generate \
+    -i /local/spec.yaml \
+    -g php-symfony \
+    -o /local/out/php-symfony
+
+# chown -R adrien:adrien /home/adrien/Projets/vscode-extension-pomodoro/app/src/UI/HTTP/REST/Swagger/out
