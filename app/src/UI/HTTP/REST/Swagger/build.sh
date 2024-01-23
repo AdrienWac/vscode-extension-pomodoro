bfb06bc52095bdb246e2856ddf499b6d9a44e31b
Adrien <adrienlambert725@msn.com>
2024-01-23 18:23:51 +0100

    feat(create-api) Edit user in docker container for generate api with openapi generator

--- app/src/UI/HTTP/REST/Swagger/build.sh
+++ app/src/UI/HTTP/REST/Swagger/build.sh
@@ -1,5 +1,5 @@
 # Build source with OpenAPI generator. Use Docker image.
-docker run --rm -v "/home/adrien/Projets/vscode-extension-pomodoro/app/src/UI/HTTP/REST/Swagger:/local" openapitools/openapi-generator-cli generate \
+docker run --rm --user $(id -u):$(id -g) -v "/home/adrien/Projets/vscode-extension-pomodoro/app/src/UI/HTTP/REST/Swagger:/local" openapitools/openapi-generator-cli generate \
     -i /local/spec.yaml \
     -g php-symfony \
     -o /local/out/php-symfony
